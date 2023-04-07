fe1f23e8b6058177185be9f676b40641e17be385
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-04-07 15:12:11 +0900

    main merge

---
+++ app/src/main/java/com/b3lon9/app/controlmaster/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.b3lon9.app.controlmaster
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
