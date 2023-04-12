fb67e3ddfccafa888adff2dc956569375befb894
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-04-12 16:24:02 +0900

    setting Theme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ControlMaster
 comfortable bring to you :)
+
+```text
+[계획]
+
+- View를 팝업창을 손으로 끌듯이 끌 수 있게 만듦 : (후순위) - MVVM구조 설정이 최우선
+    - 기능이 완성된 이후에 Float or Dialog or Service형태로 변환
+- 테마설정 : (2023.04.12)
+- View - ViewModel - Model 내부 코드 정리
+    - 밝기
+    - 음량
+    
+- Firebase연동
+```
--- app/src/main/java/com/b3lon9/app/controlmaster/MainActivity.kt
+++ app/src/main/java/com/b3lon9/app/controlmaster/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.b3lon9.app.controlmaster
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.b3lon9.app.controlmaster.databinding.ActivityMainBinding
@@ -10,9 +14,9 @@ import com.b3lon9.app.viewmodels.VolumeViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.bvm = BrightViewModel()
         binding.vvm = VolumeViewModel()
